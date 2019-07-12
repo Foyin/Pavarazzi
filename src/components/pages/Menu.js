f74157a5314202ab4012cac58979176e24f51528
Foyin Ogbara <foyinogb@live.co.uk>
2019-07-12 16:13:14 -0400

    Update Menu.js

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -13,7 +13,7 @@ function MenuMessage(props) {
 }
 
 function Nav(props) {
-  return  ( <a href={props.target}><div className="menuItem" id={props.id}>{props.name}</div></a> );
+  return  ( <a onTouchStart={} href={props.target}><div className="menuItem" id={props.id}>{props.name}</div></a> );
 }
 
 function MenuHeader(props) {
