18191e94f313669db86d6057797bcc8a0d97f990
Foyin Ogbara <foyinogb@live.co.uk>
2019-09-23 23:38:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
             
         /* If the object is completely visible in the window, fade it it */
         if( bottom_of_window > bottom_of_object ){   
-            $(this).animate({'opacity':'1'},500);
+            $(this).animate({'opacity':'1'}, 2000);
         }
     });
   }).scroll(); //invoke scroll-handler on page-load
